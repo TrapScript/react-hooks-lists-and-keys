00035f75eb57a1175b2a3acdb545ee318dd92f51
Neon Ratemo <neon.ratemo@student.moringaschool.com>
2023-07-29 00:04:25 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ColorList from "./ColorList";
+import ColorItem from "./ColorItem";
 
+;
 function App() {
   return (
     <div>
       <ColorList />
+      <ColorItem color={'orange'} />
     </div>
   );
 }
